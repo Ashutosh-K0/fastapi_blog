61aa9f47a828b88d9a554da8229ec3ef823de4b8
Ashutosh-K0 <ashutosh.kumar2022b@vitstudent.ac.in>
2026-06-05 20:58:15 +0530

    Frontend HTML Commit

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-#app object will be used to define all our routes.
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 app = FastAPI()
 
-#Data for Testing Purpose
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
 posts: list[dict] = [
     {
         "id": 1,
@@ -21,14 +25,17 @@ posts: list[dict] = [
     },
 ]
 
-#Starter API endpoint: Home Route("/")
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
-def hello():
-    return f"<h1>{posts[0]['title']}</h1>"
 
-#API endpoint to view the data from the List[posts]
-@app.get("/view")
-def view_data():
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
+def home(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "home.html",
+        {"posts": posts, "title": "Home"},
+    )
+
+
+@app.get("/api/posts")
+def get_posts():
     return posts
-
